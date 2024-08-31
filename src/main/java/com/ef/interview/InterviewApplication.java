c632ab47a8104ca65321fbc11fe00d32f4404266
Sneha Aitha <sneha.aitha25@gmail.com>
2024-08-31 20:37:57 +0400

    gym management

--- src/main/java/com/ef/interview/InterviewApplication.java
+++ src/main/java/com/ef/interview/InterviewApplication.java
@@ -2,20 +2,12 @@ package com.ef.interview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class InterviewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InterviewApplication.class, args);
-	}
-	
-	@Bean
-	//@LoadBalanced
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
 	}
 
 }
